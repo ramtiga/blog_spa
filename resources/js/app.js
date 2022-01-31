aba2d0a093fc0ed5e46f393ca0e233ff340902fc
dhane31 <dhanegm731@gmail.com>
2022-01-31 15:30:53 +0900

    add show, edit, create template

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,52 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
+import TaskEditComponent from "./components/TaskEditComponent";
 
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      //component: TaskListComponent,
+    },
+    {
+      path: '/tasks',
+      name: 'task.list',
+      component: TaskListComponent,
+    },
+    {
+      path: '/tasks/create',
+      name: 'task.create',
+      component: TaskCreateComponent,
+    },
+    {
+      path: '/tasks/:taskId',
+      name: 'task.show',
+      component: TaskShowComponent,
+      props: true,
+    },
+    {
+      path: '/tasks/:taskId/edit',
+      name: 'task.edit',
+      component: TaskEditComponent,
+      props: true,
+    },
+  ]
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +61,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +72,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
